76a530fbff6149f31a86faf729a3fec453194eb8
TJ Gilbrough <tgilbrough@yahoo.com>
2016-12-17 16:43:32 -0800

    Working classifier at around 85% with ImageDataGenerator

--- CatsvsDogs_DeepNN.py
+++ CatsvsDogs_DeepNN.py
@@ -1,6 +1,7 @@
 import os
 import re
 import csv
+import sys
 import numpy as np
 from random import shuffle
 import matplotlib.pyplot as plt
@@ -8,20 +9,24 @@ import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
 from keras.utils import np_utils
+from keras.preprocessing.image import ImageDataGenerator
 
-from Keras_Functions import save_model, load_model, img_to_matrix
+from Keras_Functions import save_model_to_disk, load_model_from_disk, img_to_matrix
 
 # For reproducibility
 np.random.seed(1337)
 
 # setting training parameters
-batch_size = 200
+batch_size = 50
 nb_classes = 2
-nb_epoch = 30
+nb_epoch = 100
 
 # Flag if the model should be loaded or trained
 load_model_flag = True
+if '-train' in sys.argv:
+    load_model_flag = False
 
 # Set resizing image dimensions
 img_rows, img_cols = 100, 100
@@ -30,7 +35,7 @@ img_rows, img_cols = 100, 100
 input_shape = (img_rows, img_cols, 1)
 
 # Get training and validation data
-img_dir = "train/"
+img_dir = 'train/'
 image_files = [img_dir + f for f in os.listdir(img_dir)]
 shuffle(image_files)
 train_size = 5 * len(image_files) // 6
@@ -38,14 +43,14 @@ train_images = image_files[:train_size]
 test_images = image_files[train_size:]
 
 X_train = [img_to_matrix(train_image, img_rows, img_cols) for train_image in train_images]
-Y_train = [0 if "cat" in f.split('/')[-1] else 1 for f in train_images]
+Y_train = [0 if 'cat' in f.split('/')[-1] else 1 for f in train_images]
 
-print("Loaded training data")
+print('Loaded training data')
 
 X_test = [img_to_matrix(test_image, img_rows, img_cols) for test_image in test_images]
-Y_test = [0 if "cat" in f.split('/')[-1] else 1 for f in test_images]
+Y_test = [0 if 'cat' in f.split('/')[-1] else 1 for f in test_images]
 
-print("Loaded testing data")
+print('Loaded testing data')
 
 # Convert data into numpy arrays
 X_train = np.array(X_train)
@@ -54,8 +59,6 @@ X_test = np.array(X_test)
 # Normalize the pixels to be in the range of [0, 1]
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
 
 # Cocnvert class vectors to binary class matrices
 Y_train = np_utils.to_categorical(Y_train, nb_classes)
@@ -63,8 +66,9 @@ Y_test = np_utils.to_categorical(Y_test, nb_classes)
 
 # Either train new model or load old one
 if load_model_flag:
-    model = load_model()
+    model = load_model_from_disk()
 else:
+    print("Setting up model")
     model = Sequential()
 
     model.add(Convolution2D(8, 9, 9, input_shape=input_shape))
@@ -94,25 +98,50 @@ else:
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
 
-    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-    model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(X_test, Y_test))
-    save_model(model)
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='binary_crossentropy', optimizer= sgd, metrics=['accuracy'])
+
+    print("Setting up image generator")
+
+    # Create data generator
+    datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=20,
+        horizontal_flip=True)
+
+    print("Fitting image generator")
+
+    # compute quantities required for featurewise normalization
+    # (std, mean, and principal components if ZCA whitening is applied)
+    datagen.fit(X_train)
+
+    print("Fitting model")
+
+    # fits the model on batches with real-time data augmentation
+    model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size), 
+        samples_per_epoch= len(X_train),
+        nb_epoch=nb_epoch,
+        verbose=1,
+        validation_data=(X_test, Y_test))
+
+    save_model_to_disk(model)
 
 # Test our model
+print("Evaluating model")
 score = model.evaluate(X_test, Y_test, verbose=0)
-print('Test score:', score[0])
 print('Test accuracy:', score[1])    
 
 # Get prediction data
-img_dir = "test/"
+img_dir = 'test/'
 predict_images = [img_dir + f for f in os.listdir(img_dir)]
 ids = [f.split('/')[-1][:-4] for f in predict_images]
 
 X_predict = [img_to_matrix(test_image, img_rows, img_cols) for test_image in predict_images]
-
-print("Loaded prediction data")
-
 X_predict = np.array(X_predict)
+
+print('Loaded prediction data')
 
 # Normalize pixels to range of [0, 1]
 X_predict = X_predict.astype('float32')
@@ -125,3 +154,5 @@ with open('predictions.csv', 'w', newline='') as fp:
     csv_writer = csv.writer(fp, delimiter=',')
     csv_writer.writerow(['id', 'label'])
     csv_writer.writerows(zip(ids, predictions[:, 1]))
+
+print('Saved predictions to disk')
--- Keras_Functions.py
+++ Keras_Functions.py
@@ -1,33 +1,23 @@
 import numpy as np
 from PIL import Image
-from keras.models import model_from_json
+from keras.models import load_model
 
-def save_model(model):
+def save_model_to_disk(model):
     """ Take in a model and save it along with its weights to disk """
-    # write model to json file
-    model_json = model.to_json()
-
-    with open("model.json", "w") as json_file:
-        json_file.write(model_json)
-
-    # serialize weights to HDF5
-    model.save_weights("model.h5")
+    print("Saving model to disk")
+    
+    model.save('model.h5')
     
     print("Saved model to disk")
 
-def load_model():
-    # load json and create model
-    with open('model.json', 'r') as json_file:
-        loaded_model_json = json_file.read()
+def load_model_from_disk():
+    print("Loading model from disk")
 
-    model = model_from_json(loaded_model_json)
+    model = load_model('model.h5')
 
-    # load weights into new model
-    model.load_weights("model.h5")
-    
     print("Loaded model from disk")
 
-    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
     return model
 
